70989a059dcc69c8240340ad052fe548dceb0235
Lefter Andrei <andrei.lefter@student.tuiasi.ro>
2026-03-04 16:46:37 +0200

    initial setup - mai organizam dupa

---
+++ xxTEA.py
@@ -0,0 +1,6 @@
+def encrypt(message : str, key : int) -> str:
+    raise NotImplementedError("De facut GEO")
+
+def decrypt(message : str, key : int) -> str:
+    raise NotImplementedError("De facut ANDREI")
+
