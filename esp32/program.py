66c87d013f656e6c9314e3fdef44405441cfbdc1
Georgiana <200639122+Georgiana2112@users.noreply.github.com>
2026-03-09 15:26:57 +0200

    butoane + display

--- esp32/program.py
+++ esp32/program.py
@@ -1,35 +1,62 @@
+# from machine import Pin, I2C
+# import ssd1306
+# from util import do_connect
+# from config import AP_PASSWORD, AP_SSID, PORT_SEND, PORT_RCV
+# import socket
+#
+# # oled -> addr = 0x3C
+# i2c = I2C(sda=Pin(21), scl=Pin(22))
+# display = ssd1306.SSD1306_I2C(128, 32, i2c)
+#
+# # display on display :DDDD
+# # display.text('Hello, World!', 0, 0, 1)
+# # display.show()
+#
+# ip, mask = do_connect(AP_SSID, AP_PASSWORD)  # connect through wifi + addr1 -> my ip + mask :D
+#
+# addr_send = (ip, PORT_SEND)  # adrr trimitere
+# addr_rcv = (ip, PORT_RCV)  # adrr primire
+#
+# # socket trimitere
+# # socket_send = socket.socket()
+# # socket_send.connect(addr_rcv)
+#
+# # socket primire neblocant
+# socket_rcv = socket.socket()
+# socket_rcv.bind(addr_rcv)
+#
+# # socket_send.send(b'ceva')
+#
+# # while True:
+# #      data = socket_rcv.recv(4)
+# #      print(str(data, 'utf8'), end='')
+#
+#
+#
+
+
 from machine import Pin, I2C
 import ssd1306
-from util import do_connect
-from config import AP_PASSWORD, AP_SSID, PORT_SEND, PORT_RCV
-import socket
+from time import sleep_ms
+from util import print_this
 
 # oled -> addr = 0x3C
 i2c = I2C(sda=Pin(21), scl=Pin(22))
 display = ssd1306.SSD1306_I2C(128, 32, i2c)
 
+left_button = Pin(18, Pin.IN, Pin.PULL_DOWN)
+right_button = Pin(26, Pin.IN, Pin.PULL_DOWN)
+print(i2c.scan())
+
 # display on display :DDDD
-# display.text('Hello, World!', 0, 0, 1)
-# display.show()
+print_this(display, 'esp vorbeste in soapta', poz_x=0, line=1, color=1)
 
-ip, mask = do_connect(AP_SSID, AP_PASSWORD)  # connect through wifi + addr1 -> my ip + mask :D
-
-addr_send = (ip, PORT_SEND)  # adrr trimitere
-addr_rcv = (ip, PORT_RCV)  # adrr primire
-
-# socket trimitere
-# socket_send = socket.socket()
-# socket_send.connect(addr_rcv)
-
-# socket primire neblocant
-socket_rcv = socket.socket()
-socket_rcv.bind(addr_rcv)
-
-# socket_send.send(b'ceva')
-
-# while True:
-#      data = socket_rcv.recv(4)
-#      print(str(data, 'utf8'), end='')
+while (1):
+    if (left_button.value() == 1):
+        print_this(display, 'LEFT', poz_x=0, line=1, color=1)
+    if (right_button.value() == 1):
+        print_this(display, 'RIGHT', poz_x=0, line=1, color=1)
+    sleep_ms(200)
 
 
 
--- esp32/util.py
+++ esp32/util.py
@@ -1,5 +1,6 @@
 import network
 
+
 def do_connect(ssid, password):
     import machine, network
     wlan = network.WLAN()
@@ -12,5 +13,14 @@ def do_connect(ssid, password):
     print('connected')
     return wlan.ipconfig('addr4')
 
+
+def print_this(display, text, poz_x, line, color):
+    poz_y = 0
+    if (line == 1):
+        poz_y = 16
+    display.fill(0)
+    display.show()
+    display.text(text, poz_x, poz_y, color)
+    display.show()
 
 
