3bccba5100eb102ff4c84e5a022ca368f5cd043d
Georgiana <200639122+Georgiana2112@users.noreply.github.com>
2026-03-23 15:40:52 +0200

    shrek exemplu

---
+++ xtea_util.py
@@ -0,0 +1,24 @@
+def rjust(message: str, ch: str, length: int) -> str:
+    return message + ch * (length - len(message))
+
+def ljust(message, ch, length) -> str:
+    return ch * (length - len(message)) + message
+
+def lstrip(message, ch):
+    for i in range (len(message)):
+        if message[i] != ch:
+            break
+    message = message[i:]
+    return message
+
+def rstrip(message, ch):
+    for i in range (len(message) - 1, 0, -1):
+        if message[i] != ch:
+            break
+    message = message[0:i+1]
+    return message
+
+mes = "HUNEDOARA"
+mes = rjust(mes, '\0',64)
+print(mes)
+print(rstrip(mes, '\0'))
